2ca469cc8799942337c6a7f24aca814042fa9e52
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2024-08-22 10:34:13 +0300

    Initial Commit

---
+++ DoctorsTest/DoctorsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorsTestApp.swift
+//  DoctorsTest
+//
+//  Created by Сергей Зайцев on 22.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DoctorsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
